f3403efb7bed03adb31695325d2e182b9d579d50
Mateus-lang <143464157+Mateus-lang@users.noreply.github.com>
2024-03-21 22:19:55 -0300

    feat: add initial booking component

--- barbearia/app/(home)/page.tsx
+++ barbearia/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import {format} from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale/pt-BR";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 
 export default function Home() {
@@ -22,6 +23,10 @@ export default function Home() {
         <Search />
       </div>
 
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem/>
+      </div>
     </div>
   );
 }
